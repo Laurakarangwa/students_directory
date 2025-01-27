34fc8928b7ae97502123ad4873c8685cfe0507d6
Laurakarangwa <l.kwizera1@gmail.com>
2025-01-27 11:25:18 +0200

    Hello, I have updated the repository

---
+++ the_coder.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#The following line is for creating a directory called "Student_directory"
+mkdir student_directory
+#In the directory above, we are adding three text files using the following commmand 
+touch student/cohort-1.txt
+touch student/cohort-2.txt
+touch student/cohort-3.txt
