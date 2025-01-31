d6f5ea60c080ebfde715a3f67294a4e598e80081
Laurakarangwa <l.kwizera1@gmail.com>
2025-01-31 14:11:56 +0530

    students_directory

--- the_coder.sh
+++ the_coder.sh
@@ -5,16 +5,16 @@ mkdir students_directory
 #In the directory above, we are adding three text files using the following commmand 
 touch students_directory/cohort-1.txt
 touch students_directory/cohort-2.txt
-<<<<<<< HEAD
-touch students_directory/cohort-3.txt
-#I am going to add names in cohort-3
+touch students_directory/cohort-3.txt
+#I am going to add names in cohort-3.txt
 echo "Elyse" >> students_directory/cohort-3.txt
 echo "Teta" >> students_directory/cohort-3.txt
 echo "Christian" >> students_directory/cohort-3.txt
-=======
-touch students_directory/cohort-3.txt
-#I am going to write a name in cohort-1
+#I am going to write a name in cohort-1.txt
 echo 'Santiana Kaze' >> students_directory/cohort-1.txt
 echo 'Ketsia Teta' >> students_directory/cohort-1.txt
 echo 'Manzi Arnaud' >> students_directory/cohort-1.txt
->>>>>>> ef7c2d2d0117317f34f877166add80774ec08400
+#I am going to write names in cohort-2.txt
+echo 'Jean Marie' >> students_directory/cohort-2.txt
+echo 'Karangwa' >> students_directory/cohort-2.txt
+echo 'Titi' >> students_directory/cohort-2.txt
